7d5b0c9e79eff2faa6d51d657f3fcee298850888
Kenan SUN <kenansun0@gmail.com>
2014-07-26 20:14:18 +0200

    [26/07/2014] SPI supports CPOL and CPHA settings

--- drivers/spi/spi.h
+++ drivers/spi/spi.h
@@ -2,7 +2,9 @@
 #define _SPI_H_
 #include "reg51.h"
 #include "types.h"
+#include "intrins.h"
 // Mode
+#define SPI_MODE 0
 #define CPOL 0
 #define CPHA 0
 // Port
@@ -10,12 +12,27 @@ sbit MOSI = P1^3;
 sbit MISO = P1^4;
 sbit SCLK = P1^2;
 sbit CS = P1^5;
-// Macro
+// Default SPI_MODE=0
+#ifndef SPI_MODE
+#define SPI_MODE 0
+#endif 
+// Default CPOL=0
+#ifndef CPOL
+#define CPOL 0
+#endif 
+// Default CPHA=0
+#ifndef CPHA
+#define CPHA 0
+#endif
+// Test
+#if SPI_MODE != (CPOL*2+CPHA)
+#error "SPI_MODE not match CPOL and CPHA."
+#endif
+// APIs
 #define SPI_Init() do{SCLK=CPOL;MISO=1;CS=1;}while(0)
 #define SPI_SelectDevice()   CS=0
 #define SPI_DeselectDevice() CS=1
-// Methods
-void SPI_Write(uint8_t dat);
-uint8_t SPI_Read(void);
+void SPI_WriteByte(uint8_t dat);
+uint8_t SPI_ReadByte(void);
 
 #endif // _SPI_H_
