48231c5d74cb9268f6789247219c3f3df47a7a37
Kenan SUN <kenansun0@gmail.com>
2014-07-26 18:24:41 +0200

    [26/07/2014] Add 24LC i2c eeprom; Add i2c driver

---
+++ drivers/eeprom/24LC_i2c/24LC.h
@@ -0,0 +1,15 @@
+#ifndef _24LC_H_
+#define _24LC_H_
+// Includes
+#include "i2c.h"
+#include "types.h"
+#include "delay.h"
+// Configurations
+#define DEVICE_ADDR 0x50U
+
+// APIs
+#define EEPROM_24LC_Init() I2C_Init()
+uint8_t EEPORM_24LC_ReadByte(uint16_t addr);
+void EEPORM_24LC_WriteByte(uint16_t addr, uint8_t dat);
+
+#endif // _24LC_H_
