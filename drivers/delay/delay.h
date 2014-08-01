2041fce4133beeab76c8b378723428895b774794
Kenan SUN <kenansun0@gmail.com>
2014-08-01 02:09:49 +0200

    [01/08/2014] Add DelayUs for delay.h

--- drivers/delay/delay.h
+++ drivers/delay/delay.h
@@ -4,10 +4,15 @@
 //                  Includes
 // --------------------------------------------------
 #include "types.h"
+#include "intrins.h"
 // --------------------------------------------------
 //                    APIs
 // --------------------------------------------------
 #define CPU_CORE_FREQUENCY 12000000UL
 void DelayMs(unsigned int num);
-
+#define Delay5Us()   do{_nop_();_nop_();_nop_();_nop_();_nop_();}while(0)
+#define Delay10Us()  do{Delay5Us();Delay5Us();}while(0)
+#define Delay50Us()  do{Delay10Us();Delay10Us();Delay10Us();Delay10Us();Delay10Us();}while(0)
+#define Delay100Us() do{Delay50Us();Delay50Us();}while(0)
+#define Delay500Us() do{Delay100Us();Delay100Us();Delay100Us();Delay100Us();Delay100Us();}while(0)
 #endif // _DELAY_H
